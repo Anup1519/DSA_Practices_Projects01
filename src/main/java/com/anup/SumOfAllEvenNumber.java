0898262b0edc918d7259d63cb9957e5a947a8d4e
Anup1519 <anupraj1519@gmail.com>
2024-01-18 13:01:57 +0530

    sum of all even number

---
+++ src/main/java/com/anup/SumOfAllEvenNumber.java
@@ -0,0 +1,13 @@
+package com.anup;
+
+public class SumOfAllEvenNumber {
+    public static void main(String args[]){
+        int sum = 0;
+        for(int i=0; i<=100; i++){
+            if(i%2==0){
+                sum+=i;
+            }
+        }
+        System.out.println("The sum of all even numbers between 1 to 100 is  " +sum);
+    }
+}
