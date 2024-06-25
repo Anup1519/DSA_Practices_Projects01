7d41686c46c783326fae43614eb5ec8974af840c
Anup1519 <anupraj1519@gmail.com>
2024-06-25 23:53:24 +0530

    Single element of array

---
+++ src/main/java/com/anup/SingleElement.java
@@ -0,0 +1,28 @@
+package com.anup;
+
+public class SingleElement {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{2,1,2,1,3};
+
+//        case 1 Time Complexity O(n^2) and Space Complexity O(1)
+        for (int i =0; i< arr.length; i++){
+            for (int j=0; j<arr.length; j++){
+                if (arr[i]==arr[j] && arr[i] != -1 && i != j){
+                    arr[i]=-1;
+                    arr[j]=-1;
+                }
+            }
+            if (arr[i]!= -1){
+                System.out.println(arr[i]);
+            }
+        }
+//        case 2  Linear TC O(n) SC(1)
+        int ans = 0;
+        for (int num : arr){
+            ans = num^ans;    // using XOR if match then 0 and not match 1
+        }
+        System.out.println(ans);
+    }
+}
